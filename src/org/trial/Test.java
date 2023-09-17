22740c3d79c16f9b69eb01cfc9f5e1aebe88a709
vith2294 <vivekmech012@gmail.com>
2023-09-17 19:16:09 +0530

    merge done

--- src/org/trial/Test.java
+++ src/org/trial/Test.java
@@ -22,6 +22,16 @@ public class Test {
 		System.out.println(values);
 
 	}
+	
+	private void index() {
+		List<Integer> var = new LinkedList<>();
+				
+				var.add(10);
+				var.add(20);
+				var.add(30);
+				var.add(90);
+				System.out.println(var);
+			}
 
 	private void list() {
 		List<Integer> ls = new ArrayList<>();
@@ -31,21 +41,13 @@ public class Test {
 		System.out.println(ls);
 	}
 
-	private void index() {
-		List<Integer> var = new LinkedList<>();
-
-		var.add(10);
-		var.add(20);
-		var.add(30);
-		var.add(90);
-		System.out.println(var);
-	}
-
 	public static void main(String[] args) {
 		Test fn = new Test();
 		fn.map();
-		fn.list();
+
 		fn.index();
+
+		fn.list();
 
 	}
 }
